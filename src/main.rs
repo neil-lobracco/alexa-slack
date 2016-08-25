5522bc9e55cd1e6139ff1f8e3f9af071c4be2d56
Neil LoBracco <neil.lobracco@gmail.com>
2016-08-24 20:23:46 -0400

    updates for reminders.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ extern crate alexa;
 extern crate hyper;
 use iron::prelude::*;
 use router::Router;
+use std::collections::BTreeMap;
+use std::borrow::Cow;
+
+static API_KEY: &'static str = "";
 
 struct RequestHandler{}
 impl alexa::RequestHandler for RequestHandler {
@@ -20,8 +24,7 @@ impl alexa::RequestHandler for RequestHandler {
                         }
                     },
                     "SetReminder" => {
-                        println!("{:?}",ir.slots);
-                        i_dont_understand()
+                        set_reminder(&ir.slots)
                     },
                     _ => i_dont_understand(),
                 }
@@ -30,23 +33,40 @@ impl alexa::RequestHandler for RequestHandler {
         }
     }
 }
-fn doubled_number_response<'a>(num: f64) -> alexa::Response<'a> {
+fn set_reminder<'a>(slots: &BTreeMap<String,String>) -> alexa::Response<'a> {
+    let at_time = match slots.get("at_time"){
+        Some(t) => t,
+        _ => { return i_dont_understand(); }
+    };
+    let reminder = match slots.get("reminder"){
+        Some(r) => r,
+        _ => { return i_dont_understand(); }
+    };
+    let res = slack_api::reminders::add(&get_client(),API_KEY,reminder,at_time,None);
+    println!("{:?}",res);
+    if res.is_ok() {
+        respond_with_text(format!("Ok, I set a reminder to {} for {}",reminder,at_time).into())
+    } else {
+        respond_with_text("Oh no, something went wrong!".into())
+    }
+}
+fn respond_with_text(txt: Cow<str>) -> alexa::Response {
         alexa::Response {
             session_attributes: None,
             card: None,
             reprompt: None,
-            output_speech: Some(alexa::OutputSpeech::Text(format!("Double {} is {}",num,num * 2f64).into())),
+            output_speech: Some(alexa::OutputSpeech::Text(txt)),
             should_end_session: true,
         }
 }
+fn get_client() -> hyper::client::Client {
+    hyper::client::Client::new()
+}
+fn doubled_number_response<'a>(num: f64) -> alexa::Response<'a> {
+        respond_with_text(format!("Double {} is {}",num,num * 2f64).into())
+}
 fn i_dont_understand<'a>() -> alexa::Response<'a> {
-        alexa::Response {
-            session_attributes: None,
-            card: None,
-            reprompt: None,
-            output_speech: Some(alexa::OutputSpeech::Text("Oh no, I don't understand what you said!".into())),
-            should_end_session: true,
-        }
+    respond_with_text("Oh no, I don't understand what you said!".into())
 }
 fn main() {
     let mut router = Router::new();
@@ -55,31 +75,11 @@ fn main() {
     let ih = alexa::IronHandler::new("amzn1.ask.skill.b5e47314-7712-4fbf-aece-3038cbd9a5d4".to_owned(),Box::new(rh));
     router.any("/",ih);
     Iron::new(router).http("0.0.0.0:3000").unwrap();
-    //let input = env::args().last().unwrap();
-    //let num = input.parse::<f64>().unwrap();
-    //println!("{}",num * (2 as f64));
-    //let client = hyper::Client::new();
-    //println!("{:?}",slack_api::channels::list(&client,"",Some(true)));
     
 }
 fn handle_healthcheck(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((iron::status::Ok)))
 }
-/*
-fn handleDNRequest(ir: &IntentRequest) -> IronResult<Response> {
-    let answer = match ir.slots.get("num") {
-        Some(ref s) => {
-            let num: f64 = s.parse().unwrap();
-            let doubled = num * 2.0f64;
-            format!("Double {} is {}",num,doubled)
-        },
-        _ => "I don't understand".to_owned(),
-    };
-    let response = AlexaResponse::new(&answer);
-    let encoded = serde_json::to_string(&response).unwrap();
-    Ok(Response::with((iron::status::Ok, encoded)))
-}
-*/
 
 
 #[cfg(test)]
